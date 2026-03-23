0729a28cf1c0448dbf6759b09cc6a3fbdd61545b
kemboi Amos <kemboiamos@gmail.com>
2026-03-23 20:08:34 +0300

    Update to allow user enter inputs

--- QuickSortDesc.cpp
+++ QuickSortDesc.cpp
@@ -8,7 +8,7 @@ int partition(int arr[], int low, int high) {
     int j;
 
     for(j = low; j <= high - 1; j++) {
-        if (arr[j] > pivot) {
+        if (arr[j] > pivot) {   
             i++;
             swap(&arr[i], &arr[j]);
         }
@@ -28,19 +28,38 @@ void quickSort(int arr[], int low, int high) {
 }
 
 void swap(int* a, int* b) {
-    int t = *a;
+    int temp = *a;
     *a = *b;
-    *b = t;
+    *b = temp;
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = 6;
-    int i;
+    int n, i;
 
+    //Input size of the array
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];   
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    //Display unsorted array
+    printf("\nUnsorted array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    // Sort
     quickSort(arr, 0, n - 1);
 
-    for (i = 0; i < n; i++) {
+    //Display sorted array
+    printf("\nSorted array in Descending order:\n");
+    for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
 
